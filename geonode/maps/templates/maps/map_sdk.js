2407f76fcbfcc22159e474f490a5358ec9758624
Bart van den Eijnden <bartvde@opengeo.org>
2013-01-10 14:20:01 +0100

    first pass at integrating a Suite SDK app in GeoNode

---
+++ geonode/maps/templates/maps/map_sdk.js
@@ -0,0 +1,32 @@
+{% include 'geonode/ext_header.html' %}
+{% include 'geonode/sdk_header.html' %}
+<script type="text/javascript">
+var app;
+Ext.onReady(function() {
+{% autoescape off %}
+    var config = Ext.apply({
+        authStatus: {% if user.is_authenticated %} 200{% else %} 401{% endif %},
+        proxy: "/proxy/?url=",
+        printService: "{{GEOSERVER_BASE_URL}}pdf/",
+        /* The URL to a REST map configuration service.  This service 
+         * provides listing and, with an authenticated user, saving of 
+         * maps on the server for sharing and editing.
+         */
+        rest: "{% url maps_browse %}",
+        ajaxLoginUrl: "{% url account_ajax_login %}",
+        portalItems: [{
+            xtype: "container",
+            layout: "fit",
+            height: 81,
+            region: "north"
+        }],
+        homeUrl: "{% url home %}",
+        localGeoServerBaseUrl: "{{ GEOSERVER_BASE_URL }}",
+        localCSWBaseUrl: "{{ CATALOGUE_BASE_URL }}",
+        csrfToken: "{{ csrf_token }}"
+    }, {{ config }});
+
+    app = new MyApp.Viewer(config);
+{% endautoescape %}
+});
+</script>
