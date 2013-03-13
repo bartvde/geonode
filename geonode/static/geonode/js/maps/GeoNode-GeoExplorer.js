e4dc26ae834c4021f7f9cb0ea7387c3cf72bbcb2
Bart van den Eijnden <bartvde@opengeo.org>
2013-03-13 11:43:30 +0100

    move save plugin

--- geonode/static/geonode/js/maps/GeoNode-GeoExplorer.js
+++ geonode/static/geonode/js/maps/GeoNode-GeoExplorer.js
@@ -1,5 +1,157 @@
 Ext.ns("GeoNode.plugins");
 
+GeoNode.plugins.Save = Ext.extend(gxp.plugins.Tool, {
+
+    metadataFormCancelText : "UT:Cancel",
+    metadataFormSaveAsCopyText : "UT:Save as Copy",
+    metadataFormSaveText : "UT:Save",
+    metaDataHeader: 'UT:About this Map',
+    metaDataMapAbstract: 'UT:Abstract',
+    metaDataMapTitle: 'UT:Title',
+
+    ptype: 'gn_save',
+
+    init: function(target) {
+        GeoNode.plugins.Save.superclass.init.apply(this, arguments);
+        this.target.on("beforesave", function(requestConfig, callback) {
+            if (this._doSave === true) {
+                delete this._doSave;
+                if (this.target.id) {
+                    requestConfig.url = this.target.rest + this.target.id + "/data";
+                } else {
+                    requestConfig.url = this.target.rest + "new/data";
+                }
+                return true;
+            } else {
+                this.showMetadataForm(callback);
+                return false;
+            }
+        }, this);
+        this.target.on("beforehashchange", function(hash) {
+            return false;
+        });
+    },
+
+    addActions: function() {
+        return GeoNode.plugins.Save.superclass.addActions.apply(this, [{
+            text: 'Save',
+            handler: function() {
+                this.target.doAuthorized(["ROLE_ADMINISTRATOR"], function() {
+                    this.target.save(this.target.showEmbedWindow);
+                }, this);
+            },
+            scope: this
+        }]);
+    },
+
+    /** private: method[initMetadataForm]
+     *
+     * Initialize metadata entry form.
+     */
+    initMetadataForm: function(callback){
+
+        var titleField = new Ext.form.TextField({
+            width: '95%',
+            fieldLabel: this.metaDataMapTitle,
+            value: this.target.about.title,
+            allowBlank: false,
+            enableKeyEvents: true,
+            listeners: {
+                "valid": function() {
+                    saveAsButton.enable();
+                    saveButton.enable();
+                },
+                "invalid": function() {
+                    saveAsButton.disable();
+                    saveButton.disable();
+                }
+            }
+        });
+
+        var abstractField = new Ext.form.TextArea({
+            width: '95%',
+            height: 200,
+            fieldLabel: this.metaDataMapAbstract,
+            value: this.target.about["abstract"]
+        });
+
+        var metaDataPanel = new Ext.FormPanel({
+            bodyStyle: {padding: "5px"},
+            labelAlign: "top",
+            items: [
+                titleField,
+                abstractField
+            ]
+        });
+        metaDataPanel.enable();
+
+        var saveAsButton = new Ext.Button({
+            text: this.metadataFormSaveAsCopyText,
+            disabled: !this.target.about.title,
+            handler: function(e){
+                delete this.target.id;
+                this.target.about.title = titleField.getValue();
+                this.target.about["abstract"] = abstractField.getValue();
+                this.metadataForm.hide();
+                this._doSave = true;
+                this.target.save(this.metadataForm.saveCallback);
+            },
+            scope: this
+        });
+        var saveButton = new Ext.Button({
+            text: this.metadataFormSaveText,
+            disabled: !this.target.about.title,
+            handler: function(e){
+                this.target.about.title = titleField.getValue();
+                this.target.about["abstract"] = abstractField.getValue();
+                this.metadataForm.hide();
+                this._doSave = true;
+                this.target.save(this.metadataForm.saveCallback);
+            },
+            scope: this
+        });
+
+        this.metadataForm = new Ext.Window({
+            title: this.metaDataHeader,
+            closeAction: 'hide',
+            saveCallback: callback,
+            items: metaDataPanel,
+            modal: true,
+            width: 400,
+            autoHeight: true,
+            bbar: [
+                "->",
+                saveAsButton,
+                saveButton,
+                new Ext.Button({
+                    text: this.metadataFormCancelText,
+                    handler: function() {
+                        titleField.setValue(this.target.about.title);
+                        abstractField.setValue(this.target.about["abstract"]);
+                        this.metadataForm.hide();
+                    },
+                    scope: this
+                })
+            ]
+        });
+    },
+
+    /** private: method[showMetadataForm]
+     *  Shows the window with a metadata form
+     */
+    showMetadataForm: function(callback) {
+        if(!this.metadataForm) {
+            this.initMetadataForm(callback);
+        } else {
+            this.metadataForm.saveCallback = callback;
+        }
+        this.metadataForm.show();
+    }
+
+});
+
+Ext.preg(GeoNode.plugins.Save.prototype.ptype, GeoNode.plugins.Save);
+
 GeoNode.plugins.SaveHyperlink = Ext.extend(gxp.plugins.Tool, {
 
     ptype: 'gn_savehyperlink',
@@ -198,15 +350,6 @@ Ext.preg(GeoNode.plugins.LayerInfo.prototype.ptype, GeoNode.plugins.LayerInfo);
  */
 GeoNode.Composer = window.GeoExplorer && Ext.extend(GeoExplorer.Composer, {
 
-    /** begin i18n */
-    metadataFormCancelText : "UT:Cancel",
-    metadataFormSaveAsCopyText : "UT:Save as Copy",
-    metadataFormSaveText : "UT:Save",
-    metaDataHeader: 'UT:About this Map',
-    metaDataMapAbstract: 'UT:Abstract',
-    metaDataMapTitle: 'UT:Title',
-    /** end i18n */
-
     ajaxLoginUrl: null,
 
     /** private: method[showUrl]
@@ -248,6 +391,9 @@ GeoNode.Composer = window.GeoExplorer && Ext.extend(GeoExplorer.Composer, {
         }, {
             ptype: 'gn_savehyperlink'
             /* TODO make sure it's in the right position */
+        }, {
+            ptype: 'gn_save'
+            /* TODO make sure we use the save button from GeoExplorer instead */
         }, {
             ptype: "gn_layerinfo",
             actionTarget: ["layers.contextMenu"]
@@ -415,129 +561,6 @@ GeoNode.Composer = window.GeoExplorer && Ext.extend(GeoExplorer.Composer, {
           win.show();
           var form = win.items.get(0);
           form.items.get(0).focus(false, 100);
-    },
-    initPortal: function() {
-        this.on("beforesave", function(requestConfig, callback) {
-            if (this._doSave === true) {
-                delete this._doSave;
-                if (this.id) {
-                    requestConfig.url = this.rest + this.id + "/data";
-                } else {
-                    requestConfig.url = this.rest + "new/data";
-                }
-                return true;
-            } else {
-                this.showMetadataForm(callback);
-                return false;
-            }
-        }, this);
-        this.on("beforehashchange", function(hash) {
-            return false;
-        });
-        GeoNode.Composer.superclass.initPortal.apply(this, arguments);
-    },
-    /** private: method[initMetadataForm]
-     *
-     * Initialize metadata entry form.
-     */
-    initMetadataForm: function(callback){
-
-        var titleField = new Ext.form.TextField({
-            width: '95%',
-            fieldLabel: this.metaDataMapTitle,
-            value: this.about.title,
-            allowBlank: false,
-            enableKeyEvents: true,
-            listeners: {
-                "valid": function() {
-                    saveAsButton.enable();
-                    saveButton.enable();
-                },
-                "invalid": function() {
-                    saveAsButton.disable();
-                    saveButton.disable();
-                }
-            }
-        });
-
-        var abstractField = new Ext.form.TextArea({
-            width: '95%',
-            height: 200,
-            fieldLabel: this.metaDataMapAbstract,
-            value: this.about["abstract"]
-        });
-
-        var metaDataPanel = new Ext.FormPanel({
-            bodyStyle: {padding: "5px"},
-            labelAlign: "top",
-            items: [
-                titleField,
-                abstractField
-            ]
-        });
-        metaDataPanel.enable();
-
-        var saveAsButton = new Ext.Button({
-            text: this.metadataFormSaveAsCopyText,
-            disabled: !this.about.title,
-            handler: function(e){
-                delete this.id;
-                this.about.title = titleField.getValue();
-                this.about["abstract"] = abstractField.getValue();
-                this.metadataForm.hide();
-                this._doSave = true;
-                this.save(this.metadataForm.saveCallback);
-            },
-            scope: this
-        });
-        var saveButton = new Ext.Button({
-            text: this.metadataFormSaveText,
-            disabled: !this.about.title,
-            handler: function(e){
-                this.about.title = titleField.getValue();
-                this.about["abstract"] = abstractField.getValue();
-                this.metadataForm.hide();
-                this._doSave = true;
-                this.save(this.metadataForm.saveCallback);
-            },
-            scope: this
-        });
-
-        this.metadataForm = new Ext.Window({
-            title: this.metaDataHeader,
-            closeAction: 'hide',
-            saveCallback: callback,
-            items: metaDataPanel,
-            modal: true,
-            width: 400,
-            autoHeight: true,
-            bbar: [
-                "->",
-                saveAsButton,
-                saveButton,
-                new Ext.Button({
-                    text: this.metadataFormCancelText,
-                    handler: function() {
-                        titleField.setValue(this.about.title);
-                        abstractField.setValue(this.about["abstract"]);
-                        this.metadataForm.hide();
-                    },
-                    scope: this
-                })
-            ]
-        });
-    },
-
-    /** private: method[showMetadataForm]
-     *  Shows the window with a metadata form
-     */
-    showMetadataForm: function(callback) {
-        if(!this.metadataForm) {
-            this.initMetadataForm(callback);
-        } else {
-            this.metadataForm.saveCallback = callback;
-        }
-        this.metadataForm.show();
     }
 
 });
